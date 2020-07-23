2bebf240a19c8ad5903fa51a70e3e1ebc012641a
swami <swami@swami-PC.lan>
2020-07-23 17:46:58 +0530

    SecondCommit

---
+++ LearnAutomation/src/Assignment2/ScenerioFourr.java
@@ -0,0 +1,29 @@
+package Assignment2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ScenerioFourr {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\swami\\Downloads\\driver\\chromedriver.exe");
+	     WebDriver driver=new ChromeDriver();
+	     
+	     driver.get("http://automationpractice.com/index.php?controller=authentication");
+	     driver.manage().window().maximize();
+	 
+		driver.findElement(By.id("email")).sendKeys("premahv3@gmail.com");
+		driver.findElement(By.id("passwd")).sendKeys("Anu@123");
+		
+		driver.findElement(By.id("SubmitLogin")).click();
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ LearnAutomation/src/Assignment2/ScenerioThreee.java
@@ -0,0 +1,40 @@
+package Assignment2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class ScenerioThreee {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\swami\\Downloads\\driver\\chromedriver.exe");
+	     WebDriver driver=new ChromeDriver();
+	     
+	     driver.get("http://automationpractice.com/index.php");
+	    driver.findElement(By.linkText("More")).click();
+	    Thread.sleep(2000);
+	    WebElement Quantity= driver.findElement(By.name("qty"));
+	    Quantity.findElement(By.xpath("//a[@class='btn btn-default button-plus product_quantity_up']")).click();
+	    //Quantity.findElement(By.xpath("//a[@class='btn btn-default button-minus product_quantity_down']")).click();
+	    
+  WebElement size=driver.findElement(By.name("group_1"));
+  Select select=new Select(size);
+  select.selectByValue("3");
+
+	    driver.findElement(By.xpath("//a[@name='Orange']")).click();
+	    driver.findElement(By.xpath("//a[@name='Blue']")).click();
+	    
+	    driver.findElement(By.name("Submit")).click();
+	    
+	    driver.close();
+	
+	    
+		
+		
+	}
+
+}
